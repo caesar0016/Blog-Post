04e6f16b58896c996eb4ea4f168cc21b24025da8
caesar.dapiton <dapitoncaesar26@gmail.com>
2025-01-26 19:21:57 +0800

    Update: Placing the home route outside

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,27 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-//this is middleware that prevents guest cocming into this modules
-Route::middleware('auth')->group(function(){
-    
-    Route::view('/about','header.about')->name('about');
+Route::get('/', function () {
+    return view('posts.index');
+})->name('home');
+
+
+// Routes for authenticated users
+Route::middleware('auth')->group(function() {
+    Route::view('/about', 'header.about')->name('about');
     Route::view('/contacts', 'header.contacts')->name('contacts');
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
     
 });
 
-Route::middleware('guest')->group(function(){
-    
-    Route::get('/', function () {
-        return view('posts.index');
-    })->name('home');
-
+// Routes for guests (unauthenticated users)
+Route::middleware('guest')->group(function() {
+    // Register and Login pages
     Route::view('/register', 'auth.register')->name('register');
     Route::view('/login', 'auth.login')->name('login');
     Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);    
-
+    Route::post('/login', [AuthController::class, 'login']);
 });
+
+?>
